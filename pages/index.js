c1087bd840eef85532f6e87266b9b831bbfb1a63
tao <taosiyu1122@foxmail.com>
2026-07-23 10:39:17 +0800

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-window.ScreenPages = {
-    HomePage: window.HomePage,
-    ApprovalPage: window.ApprovalPage,
-    SupervisionPage: window.SupervisionPage,
-    LawPage: window.LawPage,
-    CreditPage: window.CreditPage,
-    CockpitPage: window.CockpitPage
-};
